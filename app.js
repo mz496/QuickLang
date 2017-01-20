f2679dfd556e39b99138df429af673715553865d
Matthew Zhu <matt.z496@gmail.com>
2017-01-20 00:14:28 -0500

    Send something

--- app.js
+++ app.js
@@ -84,6 +84,7 @@ app.get('/webhook', function(req, res) {
  */
 app.post('/webhook', function (req, res) {
   var data = req.body;
+  console.log(data);
 
   // Make sure this is a page subscription
   if (data.object == 'page') {
